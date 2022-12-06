b3c0b48b1f84055e6ea316a1fbdace293f8e443c
ChinoBu1 <58575173+ChinoBu1@users.noreply.github.com>
2022-12-06 11:27:14 +0100

    Habitaciones

--- public/script/index.js
+++ public/script/index.js
@@ -1,36 +1,43 @@
-const paranosesion = document.getElementsByClassName('nosesion');
-const parasesion = document.getElementsByClassName('sesion');
-const paraHostelero = document.getElementsByClassName('hostelero');
-const nombre = document.getElementById('nombre');
-const logout = document.getElementById('logout');
+const paranosesion = document.getElementsByClassName("nosesion");
+const parasesion = document.getElementsByClassName("sesion");
+const paraHostelero = document.getElementsByClassName("hostelero");
+const nombre = document.getElementById("nombre");
+const logout = document.getElementById("logout");
 
-logout.addEventListener('click',(_e) => {
-    localStorage.removeItem('login');
-    sessionStorage.removeItem('login');
-    location.replace('/')
-})
+const form = document.getElementById("busqueda");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const resp = await fetch("/api/persona.ts");
+  console.log(await resp.json());
+});
 
-if (localStorage.getItem('login') && !sessionStorage.getItem('login')){
-    sessionStorage.setItem('login', localStorage.getItem('login'))
+logout.addEventListener("click", (_e) => {
+  localStorage.removeItem("login");
+  sessionStorage.removeItem("login");
+  location.replace("/");
+});
+
+if (localStorage.getItem("login") && !sessionStorage.getItem("login")) {
+  sessionStorage.setItem("login", localStorage.getItem("login"));
 }
 
-if (sessionStorage.getItem('login')){
-    const Sesion = JSON.parse(sessionStorage.getItem('login'))[0];
-    Array.from(paranosesion).forEach((element) => {
-        element.style.display = 'none'
+if (sessionStorage.getItem("login")) {
+  const Sesion = JSON.parse(sessionStorage.getItem("login"))[0];
+  Array.from(paranosesion).forEach((element) => {
+    element.style.display = "none";
+  });
+  Array.from(parasesion).forEach((element) => {
+    element.style.display = "block";
+  });
+  if (Sesion.Rol == "hotelero") {
+    Array.from(paraHostelero).forEach((element) => {
+      element.style.display = "block";
     });
-    Array.from(parasesion).forEach((element) => {
-        element.style.display = 'block'
-    });
-    if (Sesion.Rol == 'hotelero'){
-        Array.from(paraHostelero).forEach((element) => {
-            element.style.display = 'block'
-        });
-    }
-    const ancor = document.createElement('a')
-    ancor.href = "/cuenta"
-    ancor.style.textDecoration = "none"
-    ancor.style.color = "black"
-    ancor.innerHTML = Sesion.Nombre + ' ' + Sesion.Apellidos
-    nombre.appendChild(ancor);
+  }
+  const ancor = document.createElement("a");
+  ancor.href = "/cuenta";
+  ancor.style.textDecoration = "none";
+  ancor.style.color = "black";
+  ancor.innerHTML = Sesion.Nombre + " " + Sesion.Apellidos;
+  nombre.appendChild(ancor);
 }
