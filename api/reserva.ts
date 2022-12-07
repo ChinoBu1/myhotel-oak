7fd73bd27de148e1ae0d8c88b4ca86148a67599b
projectmyhotel <117845166+projectmyhotel@users.noreply.github.com>
2022-12-07 17:21:04 +0100

    Post reserva incompleto

--- api/reserva.ts
+++ api/reserva.ts
@@ -10,4 +10,10 @@ export default {
       `select * from fecha_reserva where idhabitacion = idhabitacion`,
     );
   },
+  async postReserva(form: FormDataReader) {
+    const data = await form.read();
+    return await client.query(
+      `insert into fecha_reserva values (, ${data.fields.FechaEntrada}, ${data.fields.FechaSalida}, ${data.fields.DNICliente}, ${data.fields.idhabitacion})`,
+    );
+  },
 };
